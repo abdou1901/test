f528f129d54ebcf44981503061d3fc43f045cb0a
abdou1901 <110035060+abdou1901@users.noreply.github.com>
2025-06-04 00:42:39 +0100

    Add/update LC-58-length-of-last-word/solution.c

---
+++ LC-58-length-of-last-word/solution.c
@@ -0,0 +1,7 @@
+int lengthOfLastWord(char* s) {
+    int i = strlen(s) - 1;
+    while(s[i] == ' ') i--;
+    int length = 0;
+    while(i >= 0 && s[i] != ' ') {length++;i--;}
+    return length;
+}
